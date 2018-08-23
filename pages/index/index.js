75911241a7db247ed95d81c4d503590d1afc3d72
G.M <gmxie@trussan.com>
2018-08-23 18:43:54 +0800

    接入百度地图微信api

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 // pages/index/index.js
 
-
+const bmap = require('../../libs/bmap-wx.js'); 
 Page({
 
   /**
@@ -14,7 +14,26 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
- 
+    var that = this;
+    // 新建百度地图对象 
+    var BMap = new bmap.BMapWX({
+      ak: 'EPSV8GGLf7Dhq5IGRENMaH6is5joXOBI'
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      var weatherData = data.currentWeather[0];
+      weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' + '日期：' + weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' + '天气：' + weatherData.weatherDesc + '\n' + '风力：' + weatherData.wind + '\n';
+      that.setData({
+        weatherData: weatherData
+      });
+    }
+    // 发起weather请求 
+    BMap.weather({
+      fail: fail,
+      success: success
+    }); 
   },
  
   /**
